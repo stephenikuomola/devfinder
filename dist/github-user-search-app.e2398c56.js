a80ddc2f6cab3cc6f6a11fa44d2df19c4c617d70
Ikuomola Stephen <cstephenid@yahoo.com>
2026-06-08 11:57:15 +0100

    chore(core):delete dist folder

--- dist/github-user-search-app.e2398c56.js
+++
@@ -1,2 +0,0 @@
-(()=>{let e,r,t="dark",a="light";try{e=JSON.parse(localStorage.getItem("devfinder-prefs")||"{}")}catch(r){e={checkErrorMessage:r instanceof Error?r.message:String(r)}}let c=e.theme,s=globalThis.matchMedia("(prefers-color-scheme: dark)");r=c===t||c===a?c:s.matches?t:a,document.documentElement.dataset.theme=r})();
-//# sourceMappingURL=github-user-search-app.e2398c56.js.map
